23f1e4d438fd3d63d313572eb695529d4473b2dd
deniz <47602397+denizsivas@users.noreply.github.com>
2024-12-16 22:59:41 +0300

    Example use cases
    some OOP example use cases

---
+++ SweOOP.py
@@ -0,0 +1,20 @@
+class SoftwareEngineer:
+
+alias = "Keyboard Magic"
+
+def _init(self, name, age, level, salary);
+    
+self.name = name
+self.age = age
+self.level = level
+self.salary = salary
+
+se1 = ["Software Eng", "Harry", 20, Junior, 5000]
+se2 = ["Software Eng", "Julie", 37, Senior, 7000]
+se3 = ["Designer", "Filip", 50, Senior, 50000]
+
+def code(self):
+    print( f"{self.name} is writing code")
+
+se1.code()
+se2.code()
